ba8925aed3214037c4bcfea89986204c0f84bd1d
Clarence Kinuthia <mclarence7311@gmail.com>
2024-05-22 10:04:56 +0300

    solved all functions

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,45 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i>0:
+        print(i)
+        i-=1
+        
+    print ('Happy New Year!')
+  
 
-def square_integers(int_list):
-    # code goes here!
-    pass
+
+
+def square_integers(integers_list):
+    return [integer**2 for integer in integers_list]
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i=1
+    while i <= 100:
+        if (i % 3 == 0) and (i % 5 == 0):
+            print('FizzBuzz')
+        elif i % 3 == 0 :
+            print ('Fizz')
+        elif i % 5 == 0:
+            print('Buzz')
+        else:
+            print(i)
+        i+=1
+print(fizzbuzz())
+
+# i = 10
+# while i>=0:
+#     print(i)
+#     i-=1
+
+# for i in range(10):
+#     print(f'i is {i}')
+    
+
+
+# player_heights = [0.008, 0.008, 0.008, 0.009, 0.008, 0.01, 0.009, 0.009, 0.01, 0.008, 0.009, 0.009, 0.008, 0.008, 0.008, 0.009, 0.008, 0.009, 0.01, 0.01]
+
+# inch_heights = [heights * 7920 for heights in player_heights]
+
+# print(inch_heights)
